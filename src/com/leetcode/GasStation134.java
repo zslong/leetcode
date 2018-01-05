13a2a3c3e651855ecfee889aad3ac60e11da6559
shilong.zhang <shilong.zhang@sumscope.com>
2018-01-05 18:16:23 +0800

    new

---
+++ src/com/leetcode/GasStation134.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+/**
+ * Created by shilong.zhang on 2018/1/5.
+ * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+ You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+ Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+ Note:
+ The solution is guaranteed to be unique.
+ */
+public class GasStation134 {
+    class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            if (gas == null || cost == null || gas.length < 1 || cost.length < 1 || gas.length != cost.length) {
+                return 0;
+            }
+
+            int len = gas.length;
+            int i = 0;
+            int sum = 0;
+            int start = i;
+            int prevSum = 0;
+            while (i < len) {
+                int remain = gas[i] - cost[i];
+                sum += remain;
+                if (prevSum + sum < 0) {
+                    start = i+1;
+                    prevSum = sum;
+                    sum = 0;
+                }
+                i++;
+            }
+
+            if (start >= 0 || start < len) {
+                return start;
+            } else {
+                return -1;
+            }
+
+        }
+    }
+}
