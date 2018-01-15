0baa60e25102bc8d322015a5c4606f6ee9602313
shilong.zhang <shilong.zhang@sumscope.com>
2018-01-15 14:45:19 +0800

    fix Trie208

--- src/com/leetcode/Trie208.java
+++ src/com/leetcode/Trie208.java
@@ -8,39 +8,59 @@ package com.leetcode;
  */
 
 public class Trie208 {
+
+    class TrieNode {
+        public boolean isWord;
+        public TrieNode[] children = new TrieNode[26];
+        public TrieNode() {}
+    }
+
     class Trie {
 
-        class Node {
-            char[] chars = new char[26];
-            Node[] nodes = new Node[26];
-
-            Node(){
-                for (int i = 0; i < 26; i++) {
-                    chars[i] = 'a' - 1;
-                }
-            }
-        }
-
-        private Node root;
+        private TrieNode root;
 
         /** Initialize your data structure here. */
         public Trie() {
-            root = new Node();
+            root = new TrieNode();
         }
 
         /** Inserts a word into the trie. */
         public void insert(String word) {
-
+            TrieNode curNode = root;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word.charAt(i) - 'a';
+                if (curNode.children[index] == null) {
+                    curNode.children[index] = new TrieNode();
+                }
+                curNode = curNode.children[index];
+            }
+            curNode.isWord = true;
         }
 
         /** Returns if the word is in the trie. */
         public boolean search(String word) {
-
+            TrieNode curNode = root;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word.charAt(i) - 'a';
+                if (curNode.children[index] == null) {
+                    return false;
+                }
+                curNode = curNode.children[index];
+            }
+            return curNode.isWord;
         }
 
         /** Returns if there is any word in the trie that starts with the given prefix. */
         public boolean startsWith(String prefix) {
-
+            TrieNode curNode = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                int index = prefix.charAt(i) - 'a';
+                if (curNode.children[index] == null) {
+                    return false;
+                }
+                curNode = curNode.children[index];
+            }
+            return true;
         }
     }
 
@@ -51,4 +71,14 @@ public class Trie208 {
      * boolean param_2 = obj.search(word);
      * boolean param_3 = obj.startsWith(prefix);
      */
+
+    public static void main(String[] args) {
+        Trie208 trie208 = new Trie208();
+        Trie trie = trie208.new Trie();
+        trie.insert("hello");
+        System.out.println(trie.search("hello"));
+        System.out.println(trie.startsWith("he"));
+        System.out.println(trie.search("hellp"));
+        System.out.println(trie.startsWith("h"));
+    }
 }
