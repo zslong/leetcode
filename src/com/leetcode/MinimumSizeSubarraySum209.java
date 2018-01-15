5195d5f6764693c74d8a7efd2e00de7c3d1d2e18
Shilong Zhang <dragoniszhang@gmail.com>
2018-01-16 00:49:20 +0800

    add solution to 209

---
+++ src/com/leetcode/MinimumSizeSubarraySum209.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+/**
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+ For example, given the array [2,3,1,2,4,3] and s = 7,
+ the subarray [4,3] has the minimal length under the problem constraint.
+ */
+
+public class MinimumSizeSubarraySum209 {
+    class Solution {
+        public int minSubArrayLen(int s, int[] nums) {
+
+        }
+    }
+}
